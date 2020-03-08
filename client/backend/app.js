ae24abe9554a724de220fadf890759de70a914cd
markatienza <atienza.markangelo26@gmail.com>
2020-03-08 09:44:56 +0800

    change static files

--- client/backend/app.js
+++ client/backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../front/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
