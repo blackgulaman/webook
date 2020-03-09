fbaea3001cb95fb7edd92bcf8b9e851581c6af7f
markatienza <atienza.markangelo@gmail.com>
2020-03-10 07:44:42 +0800

    Reconstruct the folder and change the static path

--- client/backend/src/configs/app-config.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// Application port
-exports.port = process.env.PORT || 9000;
-
-// Checker for what server is to run (http | https)
-exports.isHttp = false;
--- client/backend/src/loaders/index.js
+++
@@ -1,7 +0,0 @@
-const log = require('../libs/winston')('loader/index.js');
-module.exports = async ({ app }) => {
-  await require('./mongodb')();
-  log.info('MongoDB is now initialized!');
-  await require('./express')({ app });
-  log.info('Express configuration is successfully loaded!');
-};
